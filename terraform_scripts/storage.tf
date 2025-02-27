8e30af062003a9cd04d6b01b4114f625ac6b6d0a
unknown <sauravkrishna1@gmail.com>
2025-02-27 14:45:36 -0800

    Terraform scripts

--- terraform_scripts/storage.tf
+++ terraform_scripts/storage.tf
@@ -4,10 +4,16 @@ resource "azurerm_storage_account" "osv_storage" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  is_hns_enabled           = true  # Enables hierarchical namespace for Data Lake Gen2
 }
 
 resource "azurerm_storage_container" "osv_container" {
   name                  = var.container_name
   storage_account_name  = azurerm_storage_account.osv_storage.name
   container_access_type = "private"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "osv_filesystem" {
+  name               = "osvfilesystem"
+  storage_account_id = azurerm_storage_account.osv_storage.id
 }
--- terraform_scripts/synapse.tf
+++ terraform_scripts/synapse.tf
@@ -1,24 +1,31 @@
+container_name = "osv-vulnerabilities-parquet"
+storage_account_name = "osvdataingestion"
+synapse_workspace_name = "osv-synapse"
+vm_public_ip = "40.87.80.202"
+saurav [ ~/osv-vulnerabilities/terraform_scripts ]$ ls
+main.tf  outputs.tf  storage.tf  synapse.tf  terraform.tfstate  terraform.tfstate.backup  variables.tf  vm.tf
+saurav [ ~/osv-vulnerabilities/terraform_scripts ]$ cat synapse.tf
 resource "azurerm_synapse_workspace" "osv_synapse" {
-  name                = var.synapse_workspace_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  storage_data_lake_gen2_filesystem_id = azurerm_storage_account.osv_storage.id
+  name                                 = var.synapse_workspace_name
+  resource_group_name                  = var.resource_group_name
+  location                             = var.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.osv_filesystem.id
   sql_administrator_login              = var.sql_admin_user
-  sql_administrator_login_password      = var.sql_admin_password
+  sql_administrator_login_password     = var.sql_admin_password
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-# Add the Synapse Spark Pool with auto-scaling
 resource "azurerm_synapse_spark_pool" "osvsparkpool" {
   name                 = "osvsparkpool"
   synapse_workspace_id = azurerm_synapse_workspace.osv_synapse.id
   node_size_family     = "MemoryOptimized"
   node_size            = "Small"
-
-  # Choose either auto-scale or fixed node count (DO NOT use both)
+        spark_version = "3.3"
   auto_scale {
     min_node_count = 3
     max_node_count = 10
   }
-
-  
 }
